829e047762e53bc8579fdc362c05a314800806af
vehlwn <36479428+vehlwn@users.noreply.github.com>
2022-06-22 17:50:24 +0400

    Add nginx reverse proxy

--- front/src/index.js
+++ front/src/index.js
@@ -5,3 +5,18 @@ import $ from "jquery";
 $("#solveBtn").on("click", async function () {
   console.log("solve");
 });
+
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+postData("https://example.com/answer", { answer: 42 }).then((data) => {
+  console.log(data); // JSON data parsed by `data.json()` call
+});
